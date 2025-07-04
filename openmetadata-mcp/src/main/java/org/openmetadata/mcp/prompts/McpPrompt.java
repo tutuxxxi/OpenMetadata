1b21356f68ff92fdf241a38d18503cafae963580
xxx_ <51317419+tutuxxxi@users.noreply.github.com>
2025-07-04 16:56:55 +0800

    Delete openmetadata-mcp directory
    delete

--- openmetadata-mcp/src/main/java/org/openmetadata/mcp/prompts/McpPrompt.java
+++
@@ -1,7 +0,0 @@
-package org.openmetadata.mcp.prompts;
-
-import io.modelcontextprotocol.spec.McpSchema;
-
-public interface McpPrompt {
-  WrappedGetPromptResult callPrompt(McpSchema.GetPromptRequest promptRequest);
-}
